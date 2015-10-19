b7e4d5f8d4d413337da975e86d187ed2ba045a9e
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-10-19 16:24:26 +0100

    Challenge 5 complete

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-ruby '2.2.1'
+ruby '2.2.3'
 
 group :test do
   gem 'rspec'
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'has an existing balance' do
     expect(subject).to respond_to(:balance)
   end
+
+  it 'increase balance by the amount specified in top-up' do
+    expect{subject.top_up(10)}.to change{subject.balance}.by(10)
+  end
 end
