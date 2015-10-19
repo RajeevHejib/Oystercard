37725374cf9f691ce1b98e029dcf1e206db1ce44
George Maddocks <gwpmad@Georges-MacBook-Pro.local>
2015-10-19 16:43:38 +0100

    Challenge 6 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "This would take the balance over #{LIMIT}" if (balance + amount) > LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,7 +5,13 @@ describe Oystercard do
     expect(subject).to respond_to(:balance)
   end
 
-  it 'increase balance by the amount specified in top-up' do
-    expect{subject.top_up(10)}.to change{subject.balance}.by(10)
+  context 'Topping up' do
+    it 'increases the balance by the amount specified in top-up' do
+      expect{subject.top_up(10)}.to change{subject.balance}.by(10)
+    end
+
+    it 'raises an error if the top-up would take the balance over 90' do
+      expect{subject.top_up(Oystercard::LIMIT + 1)}.to raise_error "This would take the balance over #{Oystercard::LIMIT}"
+    end
   end
 end
